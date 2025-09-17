7daf53806f4dced7fc4dc50c633933f33fe89b23
LT253420 <tuillierlucass@gmail.com>
2025-09-17 17:52:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
- <!-- Scripts -->
-  <script type="module">
+
     import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
     import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
